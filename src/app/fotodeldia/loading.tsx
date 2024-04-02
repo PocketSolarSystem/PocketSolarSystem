ce17579eb3dfcf6e0e1fd24c32a775b392d575c2
asanchez65016 <asanchez65016@gmail.com>
2024-04-02 13:30:21 +0200

    loading foto del dia

---
+++ src/app/fotodeldia/loading.tsx
@@ -0,0 +1,7 @@
+export default function Cargando(){
+    return (
+        <main className="flex min-h-screen flex-col justify-between p-24">
+            <p>Cargando...</p>
+        </main>   
+    )
+}
