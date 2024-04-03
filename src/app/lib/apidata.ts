b981a384bf78a216b49caedc57512d269bbe3008
asanchez65016 <91877443+asanchez65016@users.noreply.github.com>
2024-04-03 06:26:14 +0200

    Actualizar apidata.ts

--- src/app/lib/apidata.ts
+++ src/app/lib/apidata.ts
@@ -6,8 +6,8 @@ export async function fetchFotoDelDia() {
 
     const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, {
       headers: {
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache'
+        'Cache-Control': 'no-store',
+        'Pragma': 'no-store'
       }});
     const dataJSON = data.json();
     return dataJSON;
