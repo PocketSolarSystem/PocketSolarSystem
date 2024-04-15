6b9894bcb024f8de767a5c5e118527fb6ae7b808
FuFFu24 <floalonso.abad@gmail.com>
2024-04-15 20:04:21 +0200

    acerca-de-upgrade

--- src/app/planetas/acerca-de-los-planetas/page.tsx
+++ src/app/planetas/acerca-de-los-planetas/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import "./page.css";
 
 export default function AcercaDeLosPlanetas() {
   const planetas = [
@@ -74,10 +76,26 @@ export default function AcercaDeLosPlanetas() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 pt-24 md:p-24">
-      <div className="container mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-8">Los Planetas</h1>
-        <p className="mb-4">
+    <main className="flex min-h-screen flex-col items-center p-8 pt-24 md:p-24">
+      <div className="container mx-auto pt-8 pb-8">
+        <div className="mb-8 relative">
+          <Image
+            src={"/our-solar-system.jpg"}
+            width={1200}
+            height={200}
+            alt="Landing page Saturn image"
+            className="border-2 border-solid border-black skew-y-1 z-0"
+          />
+
+          <p className="absolute hidden md:block text-4xl bg-white mt-7 p-2 text-center skew-y-1 hover:bg-gray-300 border-2 border-black right-6 bottom-6">
+            Los Planetas
+          </p>
+
+          <p className="block text-lg font-bold text-4xl md:hidden bg-white mt-3 p-2 text-center skew-y-1 hover:bg-gray-300 border-2 border-black">
+            Los Planetas
+          </p>
+        </div>
+        <p className="mb-8 mt-8">
           Nuestro sistema solar tiene ocho planetas y cinco planetas enanos,
           todos ubicados en un brazo espiral exterior de la Vía Láctea llamado
           el Brazo de Orión.
@@ -89,12 +107,11 @@ export default function AcercaDeLosPlanetas() {
           responsive={responsive}
           infinite={true}
           autoPlay={true}
-          autoPlaySpeed={2000}
+          autoPlaySpeed={5000}
           keyBoardControl={true}
-          customTransition="transform 300ms ease-in-out"
-          transitionDuration={300}
+          customTransition="transform 1000ms ease-in-out"
+          transitionDuration={1000}
           containerClass="carousel-container"
-          removeArrowOnDeviceType={["tablet", "mobile"]}
           itemClass="carousel-item-padding-40-px"
         >
           {planetas.map((planeta, índice) => (
@@ -105,31 +122,61 @@ export default function AcercaDeLosPlanetas() {
                 className="mx-auto"
               />
               <h2 className="text-xl font-semibold mt-4">{planeta.nombre}</h2>
-              <p className="text-gray-700">{planeta.descripcion}</p>
+              <p className="text-gray-700 text-sm">{planeta.descripcion}</p>
             </div>
           ))}
         </Carousel>
 
-        <div className="mt-8">
-          <h2 className="text-2xl font-bold mb-4">Exoplanetas</h2>
-          <p>Planetas fuera del sistema solar.</p>
-        </div>
-
-        <div className="mt-8">
+        <div className="m-8">
           <p>
             El sistema solar tiene ocho planetas. Moviendo hacia afuera desde el
             Sol, los planetas son: Mercurio, Venus, Tierra, Marte, Júpiter,
             Saturno, Urano y Neptuno.
           </p>
+          <br />
           <p>
             Hay cinco planetas enanos oficialmente reconocidos en nuestro
             sistema solar. En orden de distancia desde el Sol son: Ceres,
             Plutón, Haumea, Makemake y Eris.
           </p>
+          <br />
           <p>
             El sistema solar puede dividirse en tres regiones: el sistema solar
             interno, el sistema solar externo, el Cinturón de Kuiper y la Nube
             de Oort.
+          </p>
+          <br />
+          <p>
+            Los planetas internos y rocosos son Mercurio, Venus, Tierra y Marte.
+            Estos mundos también son conocidos como planetas terrestres porque
+            tienen superficies sólidas. Mercurio, Tierra y Marte están siendo
+            actualmente explorados por sondas espaciales. Dos rovers están en la
+            superficie de Marte. El rover de la NASA, Perseverance, aterrizó en
+            Marte el 18 de febrero de 2021. Tres misiones están en desarrollo
+            para regresar a Venus.
+          </p>
+          <br />
+          <p>
+            Los planetas exteriores son los gigantes gaseosos Júpiter y Saturno,
+            y los gigantes de hielo Urano y Neptuno. La nave espacial Juno de la
+            NASA está en una misión extendida en Júpiter, y la misión JUICE de
+            la ESA está en camino. La NASA también está construyendo Europa
+            Clipper y Dragonfly para explorar lunas de Júpiter y Saturno.
+          </p>
+          <br />
+          <p>
+            Más allá de Neptuno, una nueva clase de mundos más pequeños llamados
+            planetas enanos dominan, incluyendo el favorito de toda la vida,
+            Plutón. La nave espacial New Horizons de la NASA visitó Plutón en
+            2015, y actualmente está explorando el Cinturón de Kuiper más allá
+            de Plutón. Los otros planetas enanos son Ceres, Makemake, Haumea y
+            Eris.
+          </p>
+          <br />
+          <p>
+            Se han descubierto miles de planetas más allá de nuestro sistema
+            solar. Los científicos los llaman exoplanetas (exo significa "desde
+            afuera").
           </p>
         </div>
       </div>
