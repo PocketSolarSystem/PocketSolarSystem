b09aad854770ef74cdeca3a54d91f7093ec45505
asanchez65016 <asanchez65016@gmail.com>
2024-04-22 10:20:42 +0200

    Cambios planetas

--- src/app/planetas/acerca-de-los-planetas/page.tsx
+++ src/app/planetas/acerca-de-los-planetas/page.tsx
@@ -1,90 +1,23 @@
 "use client";
-
 import Image from "next/image";
-import Carousel from "react-multi-carousel";
+import { CarouselPlanetas } from "@/app/utilidades/ui/carouselPlanetas/CarouselPlanetas";
 import "react-multi-carousel/lib/styles.css";
-import "./page.css";
+import { InicioTexto } from "@/app/utilidades/ui/inicio-texto";
 
 export default function AcercaDeLosPlanetas() {
-  const planetas = [
-    {
-      nombre: "Mercurio",
-      descripcion: "El planeta más veloz.",
-      imagen: "/mercurio-planeta.jpg",
-    },
-    {
-      nombre: "Venus",
-      descripcion: "La hermana gemela sobrecalentada de la Tierra.",
-      imagen: "/venus-planeta.jpg",
-    },
-    {
-      nombre: "Tierra",
-      descripcion: "Nuestro hogar.",
-      imagen: "/tierra-planeta.jpg",
-    },
-    {
-      nombre: "Marte",
-      descripcion: "El Planeta Rojo.",
-      imagen: "/marte-planeta.jpg",
-    },
-    {
-      nombre: "Júpiter",
-      descripcion: "Rey de los planetas.",
-      imagen: "/jupiter-planeta.jpg",
-    },
-    {
-      nombre: "Saturno",
-      descripcion: "La joya del sistema solar.",
-      imagen: "/saturno-inicio.jpg",
-    },
-    {
-      nombre: "Urano",
-      descripcion: "El gigante de hielo original.",
-      imagen: "/urano-planeta.jpg",
-    },
-    {
-      nombre: "Neptuno",
-      descripcion: "El planeta más lejano en nuestro sistema solar.",
-      imagen: "/neptuno-planeta.jpg",
-    },
-    {
-      nombre: "Plutón",
-      descripcion: "Un pequeño mundo con un gran corazón.",
-      imagen: "/pluton-planeta.jpg",
-    },
-    {
-      nombre: "Exoplanetas",
-      descripcion: "Planetas más allá del sistema solar.",
-      imagen: "/pluton-planeta.jpg",
-    },
-  ];
-
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-12 pt-24 md:p-24">
-            <h1 className="text-4xl mt-4 md:mb-5 basis-40 md:basis-0 lg:basis-0">
-              Los planetas
-            </h1>
+          
+        <h1 className="text-4xl mt-4 md:mb-5 basis-40 md:basis-0 lg:basis-0">
+          Los planetas
+        </h1>
       
         <div className="mb-8 mt-4 relative">
           <Image
             src={"/our-solar-system.jpg"}
-            width={1200}
-            height={200}
+            width={900}
+            height={400}
             alt="Landing page Saturn image"
             className="border-2 border-solid border-black skew-y-1 z-0"
           />
@@ -98,45 +31,16 @@ export default function AcercaDeLosPlanetas() {
           </span> */}
 
         </div>
-
-          <p className="mb-8 mt-8">
-            El Sistema Solar está compuesto por ocho planetas y cinco planetas enanos,
-            todos ubicados en un brazo espiral exterior de la Vía Láctea llamado
-            el Brazo de Orión.
-          </p>
-
+          
+        <InicioTexto
+          titulo="El Sistema Solar"
+          texto="Está compuesto por ocho planetas y cinco planetas enanos,
+          todos ubicados en un brazo espiral exterior de la Vía Láctea llamado
+          el Brazo de Orión. Gracias a Pocket Solar System puedes navegar a través de los distintos planetas del sistema solar en tres dimensiones."
+        />
         
-        <div className="container mx-auto pt-8 pb-8">
-          <h2 className="text-2xl mt-8 mb-4 text-center">
-            <strong>DATOS DE LOS PLANETAS</strong>
-          </h2>
+        <CarouselPlanetas/>
 
-          <Carousel
-            responsive={responsive}
-            infinite={true}
-            autoPlay={true}
-            autoPlaySpeed={5000}
-            keyBoardControl={true}
-            customTransition="transform 1000ms ease-in-out"
-            transitionDuration={1000}
-            containerClass="carousel-container"
-            itemClass="carousel-item-padding-40-px"
-          >
-            {planetas.map((planeta, índice) => (
-              <div key={índice} className="bg-gray-100 p-8 rounded-lg shadow-lg text-center m-2">
-                <Image
-                  src={planeta.imagen}
-                  alt={planeta.nombre}
-                  width={300}
-                  height={300}
-                  className="mx-auto rounded-full"
-                />
-                <h2 className="text-xl mt-4">{planeta.nombre}</h2>
-                <p className="text-gray-700 text-sm">{planeta.descripcion}</p>
-              </div>
-            ))}
-          </Carousel>
-        </div>
         <div className="m-8">
           <p>
             El sistema solar tiene ocho planetas. Moviendo hacia afuera desde el
---
+++ src/app/utilidades/ui/carouselPlanetas/CarouselPlanetas.tsx
@@ -0,0 +1,110 @@
+import Carousel from "react-multi-carousel";
+import Image from "next/image";
+import "./CarouselPlanetas.css"
+
+export function CarouselPlanetas(){
+
+    const planetas = [
+        {
+            nombre: "Mercurio",
+            descripcion: "El planeta más veloz.",
+            imagen: "/mercurio-planeta.jpg",
+        },
+        {
+            nombre: "Venus",
+            descripcion: "La hermana gemela sobrecalentada de la Tierra.",
+            imagen: "/venus-planeta.jpg",
+        },
+        {
+            nombre: "Tierra",
+            descripcion: "Nuestro hogar.",
+            imagen: "/tierra-planeta.jpg",
+        },
+        {
+            nombre: "Marte",
+            descripcion: "El Planeta Rojo.",
+            imagen: "/marte-planeta.jpg",
+        },
+        {
+            nombre: "Júpiter",
+            descripcion: "Rey de los planetas.",
+            imagen: "/jupiter-planeta.jpg",
+        },
+        {
+            nombre: "Saturno",
+            descripcion: "La joya del sistema solar.",
+            imagen: "/saturno-inicio.jpg",
+        },
+        {
+            nombre: "Urano",
+            descripcion: "El gigante de hielo original.",
+            imagen: "/urano-planeta.jpg",
+        },
+        {
+            nombre: "Neptuno",
+            descripcion: "El planeta más lejano en nuestro sistema solar.",
+            imagen: "/neptuno-planeta.jpg",
+        },
+        {
+            nombre: "Plutón",
+            descripcion: "Un pequeño mundo con un gran corazón.",
+            imagen: "/pluton-planeta.jpg",
+        },
+        {
+            nombre: "Exoplanetas",
+            descripcion: "Planetas más allá del sistema solar.",
+            imagen: "/pluton-planeta.jpg",
+        },
+    ];
+    
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
+
+    return (
+        <div className="container mx-auto pt-8 pb-8">
+
+          <h2 className="text-2xl mt-8 mb-4 text-center">
+            <strong>DATOS DE LOS PLANETAS</strong>
+          </h2>
+
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={5000}
+            keyBoardControl={true}
+            customTransition="transform 1000ms ease-in-out"
+            transitionDuration={1000}
+            containerClass="carousel-container"
+            itemClass=""
+          >
+              {planetas.map((planeta, índice) => (
+                <div key={índice} className="bg-gray-100 p-8 rounded-lg shadow-lg text-center m-2">
+                  <Image
+                    src={planeta.imagen}
+                    alt={planeta.nombre}
+                    width={300}
+                    height={300}
+                    className="mx-auto rounded-full"
+                  />
+                  <h2 className="text-xl mt-4">{planeta.nombre}</h2>
+                  <p className="text-gray-700 text-sm">{planeta.descripcion}</p>
+                </div>
+              ))}
+          </Carousel>
+          
+        </div>
+    )
+}
