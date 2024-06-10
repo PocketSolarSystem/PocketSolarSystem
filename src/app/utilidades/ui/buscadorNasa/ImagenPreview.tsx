0073145ac9fb30a6fefa75c30e56a1ce540ddb83
asanchez65016 <asanchez65016@gmail.com>
2024-06-10 20:30:41 +0200

    Galeria arreglada

--- src/app/utilidades/ui/buscadorNasa/ImagenPreview.tsx
+++ src/app/utilidades/ui/buscadorNasa/ImagenPreview.tsx
@@ -6,13 +6,13 @@ export function ImagenPreview(
     {imagenUrl:string, nasaId:any}
 ){
     return(
-        <div className="relative">
+        <div className="relative h-56 w-80">
             <Image 
                 width={250}
                 height={125}
                 src={imagenUrl}
                 alt={"search photo " + nasaId}
-                className="w-full h-auto border-solid border-black/75 border rounded-lg transition hover:scale-110 hover:shadow-slate-600 hover:shadow-lg"
+                className="inset-0 h-full w-full object-cover object-center border-solid border-black/75 border rounded-lg transition hover:scale-105 hover:shadow-slate-600 hover:shadow-lg"
             />
         </div>
     );
